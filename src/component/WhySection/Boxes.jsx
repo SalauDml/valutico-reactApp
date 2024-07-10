2d3370e30ab8298f905db5fe4d7dd6887d583290
SalauDml <damolasalau23@gmail.com>
2024-07-10 23:58:26 +0100

    edited media query for tablet view

--- src/component/WhySection/Boxes.jsx
+++ src/component/WhySection/Boxes.jsx
@@ -1,26 +1,7 @@
 import React from 'react'
 import styles from './Why.module.css'
 
-// function Boxes({heading, para, why, features}) {
-//   return (
-//     <div>
-//       {why ? ( 
-//         <div className={styles.box}>
-//             <h3> {heading}</h3>
-//             <p>{para}</p>
-//         </div>
-//       ) : (
-//           <div className={styles.features}>
-//             {/* <h3>{heading}</h3>
-//             <p>{para}</p> */}
-//             <h3>Personalise your reports</h3>
-//             <p> Save hours transferring results from spreadsheet to slides, by exporting your results to Powerpoint and Word templates effortlessly.</p>
-//           </div>
-//       )
-//       }
-//     </div>
-//   )
-// }
+
 function Boxes({heading, para, why, features}) {
   return (
     <div>
